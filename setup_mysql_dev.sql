16cd3379872178456912f305a058ba7fa62d62b9
Michael Aloh <mykelaloh@gmail.com>
2023-04-19 13:56:13 -0700

    add comment

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,11 @@
  -- prepare sql for project
+ -- create database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- create user hbnb_dev with password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- grant all privilege tovuser
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
+-- grant select privilege to user
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
