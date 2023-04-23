ddbd5a3a58cc381d267e329eae9666e748033690
Dinobi <architectstan01@gmail.com>
2023-04-22 20:27:48 -0700

    AIRBNB_CLONE PROJECT

--- models/place.py
+++ models/place.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer
+from sqlalchemy import Column, String, ForeignKey, Integer, Table, Float
 import models
 from sqlalchemy.orm import relationship
 from os import getenv
