b687037cd87545cbf6941836b74b6fd27e7054df
Michael Aloh <mykelaloh@gmail.com>
2023-04-24 07:04:58 -0700

    updated

--- models/review.py
+++ models/review.py
@@ -8,8 +8,16 @@ from os import getenv
 
 class Review(BaseModel):
     """ Review classto store review information """
-    __tablename__ = "reviews"
-    
-    place_id = Column(String(60), ForeignKey('place.id'), nullable=False)
-    user_id =Column(String(60), ForeignKey('users.id'), nullable=False)
-    text = Column(String(1024), nullable=False)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "reviews"
+        place_id = Column(String(60), ForeignKey('place.id'), nullable=False)
+        user_id =Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        text = ""
+        place_id = ""
+        user_id = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Review"""
+        super().__init__(*args, **kwargs)
