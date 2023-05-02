4622470f6f55affb17402a9bd9f10274f4413ba9
Dinobi <architectstan01@gmail.com>
2023-05-02 01:51:29 -0700

    AirBnB project

--- models/state.py
+++ models/state.py
@@ -11,11 +11,12 @@ from os import getenv
 class State(BaseModel, Base):
     """ State class """
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ ='states'
+        __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship("City", cascade="all, delete", backref="state")
+        cities = relationship("City", cascade="all, delete", backref="states")
     else:
         name = ""
+        cities = ""
 
     def __init__(self, *args, **kwargs):
         """initializes state"""
