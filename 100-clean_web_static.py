6e9045c7170b997e0af77b1c44a6628ff4e0736f
architectstan01@gmail.com <113438743+Di-nobi@users.noreply.github.com>
2023-05-05 05:50:47 +0100

    Deploy

---
+++ 100-clean_web_static.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+""" A favric script that deletes out of date archives"""
+def do_clean(number=0):
+
---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" A fabric script that distributes an archive to your
+web servers"""
+from fabric.operations import local, run, put
+from fabric.api import env
+import time
+import os
+env.hosts = ["34.203.29.12", "34.227.90.80"]
+env.user = "ubuntu"
+def do_pack():
+    local("mkdir -p versions")
+    final = local("tar -cvzf versions/web_static_{}.tgz web_static/".format(time.strftime("%Y%m%d%H%M%S")))
+    if final.failed:
+        return None
+    return final
+
+def do_deploy(archive_path):
+    """Deploying an archive to server"""
+    if not os.path.exists(archive_path):
+        return False
+    filed = archive_path[9:]
+    new_version = "/data/web_static/releases/" + filed[:-4]
+    filed = "/tmp/" + filed
+    #uploading of files
+    put(archive_path, '/tmp/')
+    run("sudo mkdir -p {}".format(new_version))
+    run("sudo tar -xzf {} -C {}/".format(filed, new_version))
+    run("sudo rm /tmp/{}.tgz".fomat(filed))
+    run("sudo mv {}/web_static/* {}".fotmat(filed))
+    run("sudo rm -rf {}/web_static".format(filed))
+    run("sudo rm -rf /data/web_static/current")
+    run("sudo ln -s {} /data/web_static/current".format(filed))
+
+    print("New version deployed!")
+    return True
+
+return False
