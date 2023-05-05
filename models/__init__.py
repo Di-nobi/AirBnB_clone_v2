3fd91b67e13549148e1061651897f93f5e63fdfd
architectstan01@gmail.com <113438743+Di-nobi@users.noreply.github.com>
2023-05-05 04:11:50 +0100

    Fabric Deploy

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-<<<<<<< HEAD
-=======
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
@@ -11,7 +9,6 @@ from models.city import City
 from models.place import Place
 from models.review import Review
 from models.amenity import Amenity
->>>>>>> 47b17018372f604fe72ac18c9cc99c1c84c0dd94
 from os import getenv
 if getenv('HBNB_TYPE_STORAGE') == "db":
     storage = DBStorage()
