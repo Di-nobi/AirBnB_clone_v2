743d05d6d6ca5d4f5212289afc537fd02b0294a8
architectstan01@gmail.com <113438743+Di-nobi@users.noreply.github.com>
2023-05-08 01:07:03 +0100

    Deployment

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,7 @@ import os
 env.hosts = ["34.203.29.12", "34.227.90.80"]
 env.user = "ubuntu"
 def do_pack():
+    """Function that compress files in an archive"""
     local("mkdir -p versions")
     final = local("tar -cvzf versions/web_static_{}.tgz web_static/".format(time.strftime("%Y%m%d%H%M%S")))
     if final.failed:
@@ -22,14 +23,14 @@ def do_deploy(archive_path):
     new_version = "/data/web_static/releases/" + filed[:-4]
     filed = "/tmp/" + filed
     #uploading of files
-    put(archive_path, '/tmp/')
+    put(archive_path, "/tmp/")
     run("sudo mkdir -p {}".format(new_version))
     run("sudo tar -xzf {} -C {}/".format(filed, new_version))
-    run("sudo rm /tmp/{}.tgz".fomat(filed))
-    run("sudo mv {}/web_static/* {}".fotmat(filed))
-    run("sudo rm -rf {}/web_static".format(filed))
+    run("sudo rm /tmp/{}.tgz".format(filed))
+    run("sudo mv {}/web_static/* {}".format(filed, filed))
+    run("sudo rm -rf {}/web_static".format(new_version))
     run("sudo rm -rf /data/web_static/current")
-    run("sudo ln -s {} /data/web_static/current".format(filed))
+    run("sudo ln -s {} /data/web_static/current".format(new_version))
 
     print("New version deployed!")
     return True
