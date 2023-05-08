9283a67bb2607ccc51682e43fc8b9aa115ff131e
architectstan01@gmail.com <113438743+Di-nobi@users.noreply.github.com>
2023-05-08 09:06:10 +0100

    $message

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,20 +8,21 @@ import os
 env.hosts = ["34.203.29.12", "34.227.90.80"]
 env.user = "ubuntu"
 def do_deploy(archive_path):
-    try:
-        """Deploying an archive to server"""
-        if not os.path.exists(archive_path):
-            return False
-        #uploading of files
-        put(archive_path, "/tmp/")
-        file = archive_path[-18:-4]
-        run("sudo mkdir -p /data/web_static/\releases/web_static_{}".format(file))
-        run("sudo tar -xzf /tmp/web_static_{}.tgz -C \ /data/web_Static/releases/web_static_{}/".format(file, file))
-        run("sudo rm /tmp/web_static_{}.tgz".format(file))
-        run("sudo mv /data/web_static/releases/web_static_{}/web_static/*\ /data/web_static/releases/web_static_{}/".format(file, file))
-        run("sudo rm -rf /data/web_Static/releases/\web_static_{}/web_static".format(file))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/\ web_static_{}/ /data/web_static/current".format(file))
-    except:
+    """Deploying to server"""
+    if not os.path.exists(archive_path):
         return False
-    return True
+    else:
+        file = archive_path[9:]
+        new_version = "/data/web_static/releases/" + file[:-4]
+        file = "/tmp" + file
+        put(archive_path, "/tmp")
+        run('sudo mkdir -p {}'.format(new_version))
+        run('sudo tar -xzf {} -C {}/'.format(file, new_version))
+        run('sudo rm {}'.format(file))
+        run('sudo mv {}/web_static/* {}'.format(new_version, new_version))
+        run('sudo rm -rf {}/web_static'.format(new_version))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {} /data/web_static/current'.format(new_version))
+        print("New version deployed")
+
+        return True
