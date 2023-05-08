a7994da3ea38ae85803a675cd6827792cda64a82
architectstan01@gmail.com <113438743+Di-nobi@users.noreply.github.com>
2023-05-08 07:26:27 +0100

    Deployment

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,31 +7,21 @@ import time
 import os
 env.hosts = ["34.203.29.12", "34.227.90.80"]
 env.user = "ubuntu"
-def do_pack():
-    """Function that compress files in an archive"""
-    local("mkdir -p versions")
-    final = local("tar -cvzf versions/web_static_{}.tgz web_static/".format(time.strftime("%Y%m%d%H%M%S")))
-    if final.failed:
-        return None
-    else:
-        return final
-
 def do_deploy(archive_path):
-    """Deploying an archive to server"""
-    if not os.path.exists(archive_path):
+    try:
+        """Deploying an archive to server"""
+        if not os.path.exists(archive_path):
+            return False
+        #uploading of files
+        put(archive_path, "/tmp/")
+        file = archive_path[-18:-4]
+        run("sudo mkdir -p /data/web_static/\releases/web_static_{}".format(file))
+        run("sudo tar -xzf /tmp/web_static_{}.tgz -C \ /data/web_Static/releases/web_static_{}/".format(file, file))
+        run("sudo rm /tmp/web_static_{}.tgz".format(file))
+        run("sudo mv /data/web_static/releases/web_static_{}/web_static/*\ /data/web_static/releases/web_static_{}/".format(file, file))
+        run("sudo rm -rf /data/web_Static/releases/\web_static_{}/web_static".format(file))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/\ web_static_{}/ /data/web_static/current".format(file))
+    except:
         return False
-    filed = archive_path[9:]
-    new_version = "/data/web_static/releases/" + filed[:-4]
-    filed = "/tmp/" + filed
-    #uploading of files
-    put(archive_path, "/tmp/")
-    run("sudo mkdir -p {}".format(new_version))
-    run("sudo tar -xzf {} -C {}/".format(filed, new_version))
-    run("sudo rm /tmp/{}.tgz".format(filed))
-    run("sudo mv {}/web_static/* {}".format(filed, filed))
-    run("sudo rm -rf {}/web_static".format(new_version))
-    run("sudo rm -rf /data/web_static/current")
-    run("sudo ln -s {} /data/web_static/current".format(new_version))
-
-    print("New version deployed!")
     return True
