e680cf5921b13746f9c69f02c7aa35ad073ec898
Dinobi <architectstan01@gmail.com>
2023-06-02 08:38:58 -0700

    Airbnb task

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -6,5 +6,13 @@ env.hosts = ['34.203.29.12', '34.227.90.80']
 env.user = "ubuntu"
 
 def do_clean(number=0):
+    """ A fabric script that deletes out of date archives """
+    number = int(number)
+    if number == 0:
+        number = 1
+    else:
+        number
 
-
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    way = '/data/web_static/releases'
+    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(way, number))
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,1 +1,8 @@
-#!/usr/bin/
+#!/usr/bin/python3
+""" Deployment with Fabric """
+import os
+import time
+from fabric.api import local
+from fabric.operations import env, put, run
+
+env.hosts = ['
